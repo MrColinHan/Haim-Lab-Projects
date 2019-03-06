0d76204856b3607e43e6a8f030585f7160d8c056
Changze Han <changze-han@uiowa.edu>
2019-03-06 16:13:47 -0600

    use regular expression to extract accession number in a random string

--- Flu/Find'X'inAAsequence(inFasta).py
+++ Flu/Find'X'inAAsequence(inFasta).py
@@ -7,6 +7,7 @@ Created on Wed Jan 16 15:12:13 2019
 """
 '''NEED TO UPDATE: cannot check 'X' if the accession number contains 'X' '''
 ''' 
+    For FLU project
     This script finds all Accession numers that contain 'X' or '?' in a AA Sequence fasta file. 
     
     INPUTS: 
@@ -23,7 +24,8 @@ outputDir = r"/Users/Han/Documents/Haim Lab(2018 summer)/1.23.19(12-15flu)/"
 outputName = "C-14-15 AccessionNumbers(withX).txt"
 # =======================================================================================================
 
-
+from re import compile
+ACCESSION_MATCHER = compile(r'[A-Za-z]{2}\d{6}|[A-Za-z]\d{5}')
 
 AAContent = [] # a list for all AA sequence
 
@@ -34,6 +36,15 @@ def readFasta(x,y):  # read the AA Sequence fasta file and then store them in li
 
 readFasta(AADir,AAContent) # execute the function
 
+def getAccessNum(string):
+    """
+    extract accession from the given string, returns the first match if there are multiple matches
+    :param string: the string to extract accession from
+    :return: the extracted accession
+    """
+    #print(string)
+    return ACCESSION_MATCHER.findall(string)[0]
+
 
 AccessionList = [] # a list for all Accession Numbers that contains 'X' or '?'
 def findX(): # find 'X' or '?' in the AA sequence and then store the accession number in a list
@@ -42,8 +53,8 @@ def findX(): # find 'X' or '?' in the AA sequence and then store the accession n
     while (i < len(AAContent)):
         if ('?' in AAContent[i]) or ('X' in AAContent[i]):# check 'X' and '?'
             count = count + 1
-            if (AAContent[i-1][1:9] not in AccessionList):# make sure there's no duplicate in the list
-                AccessionList.append(AAContent[i-1][1:9]) # if not in list, add to list
+            if (getAccessNum(AAContent[i-1]) not in AccessionList):# make sure there's no duplicate in the list
+                AccessionList.append(getAccessNum(AAContent[i-1])) # if not in list, add to list
         i += 2
     print(count) # print the amount of accession numbers that are stored
     
