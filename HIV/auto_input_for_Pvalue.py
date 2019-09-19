74f7fc099b74bf727e219d68a2b2de40a7b4d5e6
Changze Han <changze-han@uiowa.edu>
2019-09-18 22:18:04 -0500

    enable the code to automatically generate auto_input in more difficult situations

--- HIV/auto_input_for_Pvalue.py
+++ HIV/auto_input_for_Pvalue.py
@@ -8,9 +8,10 @@ Created on Wed Jul 24 03:55:09 2019
 
 import csv
 # Inputs ================================================================================================
-inputFile = r"/Users/Han/Documents/Haim_Lab(2018_summer)/7.23.19_fig4_centroids/22pos/CSV/AS_CSV.csv"
-outdir = r"/Users/Han/Documents/Haim_Lab(2018_summer)/7.23.19_fig4_centroids/22pos/CSV/separated/"
-clade = "BAS" # BNA, BEU, BKR, BAS
+inputFile = r"/Users/Han/Documents/Haim_Lab(2018_summer)/7.24.19_new_MDS/p_value_input/164_167_need_separate.csv"
+outdir = r"/Users/Han/Documents/Haim_Lab(2018_summer)/7.24.19_new_MDS/p_value_input/164_167_separated/"
+zStartsAt = 2 # Z starts at first row's index 4 (note: index starts at 0)
+pos_at = 1
 # ========================================================================================================
 
 
@@ -38,6 +39,8 @@ readCSV(inputFile,csvList)  # read input into list
 #writeCsv(test_out, outdir, out_file_name)
 
 pos = ""  # save position name
+region = "" # save region name
+clade = "" # sace clade name
 out_w = None  # save output file
 out_file_name = None # constract output file name
 
@@ -47,10 +50,12 @@ count = 0
 while i < len(csvList):
     
     try:
-        int(csvList[i][0]) # if true: then zip(i , i+1)
-        pos = csvList[i][0] # get position name
-        out_file_name = clade + "_ALL_" + pos + ".csv"  # construct output file name
-        out_w = list(map(list, zip(csvList[i][1:], csvList[i+1][1:]))) # save output content
+        int(csvList[i][pos_at]) # if true: then zip(i , i+1)
+        pos = csvList[i][pos_at] # get position name
+        clade = csvList[i][0]
+        region = csvList[i+1][0]
+        out_file_name = clade + f"_{region}_" + pos + ".csv"  # construct output file name
+        out_w = list(map(list, zip(csvList[i][zStartsAt:], csvList[i+1][zStartsAt:]))) # save output content
         writeCsv(out_w, outdir, out_file_name)
         count += 1
         print(f"{out_file_name} generated")
