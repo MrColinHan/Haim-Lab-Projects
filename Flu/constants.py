c4fbe832ed90575223307406a495a67d8d5918da
Changze Han <changze-han@uiowa.edu>
2019-11-06 14:57:54 -0600

    add a file for all constants

---
+++ Flu/constants.py
@@ -0,0 +1,36 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Nov  4 16:10:36 2019
+
+@author: Han
+"""
+
+'''
+    Constants : 1. hydropathy table for amino acids
+'''
+
+HYDROPATHY_SCORE_TABLE = {
+    'A': 0.68,
+    'C': 0.733,
+    'D': 0.19,
+    'E': 0.203,
+    'F': 1,
+    'G': 0.584,
+    'H': 0.304,
+    'I': 0.958,
+    'K': 0.403,
+    'L': 0.953,
+    'M': 0.782,
+    'N': 0.363,
+    'P': 0.759,
+    'Q': 0.376,
+    'R': 0.167,
+    'S': 0.466,
+    'T': 0.542,
+    'V': 0.854,
+    'W': 0.898,
+    'Y': 0.900,
+    'Z': 0,
+    '-': 1.5
+}
