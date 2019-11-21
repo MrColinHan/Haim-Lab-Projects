7abd93b438d1c28626ce92669e4e161f735d3d82
Changze Han <changze-han@uiowa.edu>
2019-11-21 00:17:27 -0600

    add new feature: calculate stdev on selected samples

--- Flu/flu_fd_cal.py
+++ Flu/flu_fd_cal.py
@@ -26,7 +26,7 @@ Created on Wed Sep 25 11:29:05 2019
 
 import pandas as pd
 import csv
-#import statistics 
+import statistics 
 #from constants import HYDROPATHY_SCORE_TABLE
 
 # ==========================================================================================
@@ -48,9 +48,10 @@ country_attribute_name = 'Country'  # this is the name of country attribute appe
 #option 5:
 accession_attribute_name = "Sequence Accession"  # value e.g.: ['KU591055', 'KU591039', 'KU590350']
 
-selection_name = accession_attribute_name  # choose from 5 options above
-
-selection_value = ['KU590454', 'KU592096', 'KU591140', 'KU592142', 'KX003406', 'KX413369', 'KX413331', 'KU591700', 'KU590828', 'KU591007', 'KX413665', 'KX413705', 'KX413561', 'KY044240', 'KX412630', 'KX412814', 'KX413953', 'KX413649', 'KY044247', 'KX412502', 'KX413545', 'KX413553', 'KX413737', 'KX413745', 'KX003471'] #'15-16'  
+# select identifier : 
+selection_name = group_attribute_name  # choose from 5 options above
+# select cutoff/value/... : 
+selection_value = 100 #'15-16'  
 # 1. if 'Flu Season' is selected, you can also do multiple 
 #    years : '13-16' which will combine 13-14,14-15,15-16
 # 2. if 'Group' is selected, then selection_value is the cutoff value, 
@@ -62,7 +63,7 @@ selection_value = ['KU590454', 'KU592096', 'KU591140', 'KU592142', 'KX003406', '
 # e.g: H1N1--(1,549)   H3N2--(1,550)
 position_range = (1,550) 
  
-need_stdev = True  # True : if want to calculate stdev
+need_stdev = True  # True : if want to calculate stdev  False: only calculate FD
 
 # ==========================================================================================
 
@@ -77,12 +78,13 @@ AA=['Z','N','T','S','D','E','K','R','H','Y','Q','I','L','V','A','C','F','G','M',
 
 seq_list = []  # list store seq file
 selection_list = []  # store lists after selection
-df = None  # dataframe for selection_list in FD/stdev cal
+df = None  # dataframe for selection_list in FD cal
+df_sd = None # dataframe for selection_list in stdev cal
 fd_out_list = []  # store FD output lists
 stdev_out_list = []  # store stdev output lists
 group_name_list = []  # store the distinct group names
 fd_zip_output = []  # save ziped fd out list, will be wrote in output file
-stdev_zip_output = []  # save ziped stdev out list, will be wrote in output file
+#stdev_zip_output = []  # save ziped stdev out list, will be wrote in output file
 
 
 def read_csv(filedir,listname):
@@ -137,7 +139,7 @@ def select_rows(name,value):  #select rows based on target attribute 'name' and 
         print(f"{name} index: {target_name_index}")
     # if want to select seasons: 
     if name == season_attribute_name:  
-        print(f"Year selected: \n  {generate_season_range(value)}")
+        print(f"Season selected: \n  {generate_season_range(value)}")
         season_range = generate_season_range(value)
         for i in seq_list[1:]:  # exclude header row
             if i[target_name_index] in season_range:  # check season
@@ -161,16 +163,45 @@ def select_rows(name,value):  #select rows based on target attribute 'name' and 
             if i[target_name_index] in value:
                 selection_list.append(i)
 
-def calculate_fd(x,y):  # x = position_range   e.g. (1,549); y is the title added to right corner of output
+def calculate_fd_stdev(x,y):  # x = position_range   e.g. (1,549); y is the title added to right corner of output
     global fd_out_list
     fd_out_list = []  # reset
+    global stdev_out_list
+    stdev_out_list = []  # reset 
     header_row = seq_list[0]
     #construct a dataframe for the selected lists
     global df
     df = pd.DataFrame(selection_list[1:], columns = header_row)
     
-    fd_out_list.append([f'{y}'] + AA + ['Sample#'])  # add headers for the output
+    HYDROPATHY_SCORE_TABLE = {
+            'A': 0.68,
+            'C': 0.733,
+            'D': 0.19,
+            'E': 0.203,
+            'F': 1,
+            'G': 0.584,
+            'H': 0.304,
+            'I': 0.958,
+            'K': 0.403,
+            'L': 0.953,
+            'M': 0.782,
+            'N': 0.363,
+            'P': 0.759,
+            'Q': 0.376,
+            'R': 0.167,
+            'S': 0.466,
+            'T': 0.542,
+            'V': 0.854,
+            'W': 0.898,
+            'Y': 0.900,
+            'Z': 0,
+            '-': 1.5
+            }
     
+    fd_out_list.append([f'{y}'] + AA + ['Sample#'])  # add header col for the fd output
+    stdev_out_list.append(['Sample#'] + ['Group'] + list(range(position_range[0],position_range[1]+1)))  # add header col for stdev output
+    stdev_out_list.append([len(selection_list)-1] + [f'{y}'])  # add sample number & group identifier to stdev output
+                           
     i = x[0]   # start from the first position in the position range
     while ((i >= x[0]) and (i <= x[1])):
         total_count = 0  # count the total sample number of this position
@@ -197,6 +228,7 @@ def calculate_fd(x,y):  # x = position_range   e.g. (1,549); y is the title adde
                         ,'W':0}  # store the count of each AA, reset for each position
         aa_count_list = []  # store aa count(values of dict) to a list, first element is the position
         aa_pct_list = []  # store the percentages of aa to a list
+        hyd_stdev_list = []  # store the hydropathy value list of this position
         
         aa_list = df[str(i)].tolist()  # use dataframe to extract the column of this position
         
@@ -204,9 +236,10 @@ def calculate_fd(x,y):  # x = position_range   e.g. (1,549); y is the title adde
         for j in aa_list:
             if j in list(aa_count_dic.keys()):   # only count AA, ignore '?' '-'
                 aa_count_dic[j] = aa_count_dic[j] + 1  # count the AA and add number in dict
+            if j in list(HYDROPATHY_SCORE_TABLE.keys()):  # hydropathy value doesn't have '?' '-' 
+                hyd_stdev_list.append(HYDROPATHY_SCORE_TABLE[j])  # assign a hydro value to this AA and add add the value to list
         
-        #print(len(list(aa_count_dic.values())))
-        #print(list(aa_count_dic.values()))
+
         
         pure_aa_list = list(aa_count_dic.values())  # transfer the value of dict to a list
         total_count = sum(list(aa_count_dic.values()))  # total sample count of this position
@@ -214,65 +247,24 @@ def calculate_fd(x,y):  # x = position_range   e.g. (1,549); y is the title adde
         aa_count_list = pure_aa_list[:]
         aa_count_list.insert(0,i)  # add position to the front of the list
         aa_count_list.append(total_count)  # add the total sample number of this position to the end
-        
         # calculate percentage
         for k in pure_aa_list:
             if total_count != 0:
                 aa_pct_list.append(k/total_count*100)  # calculate %
             else:
                 aa_pct_list.append(0)
-        
         aa_pct_list.insert(0,i)
         aa_pct_list.append(total_count)
         
         fd_out_list.append(aa_pct_list)  # add to the output list
-        #fd_out_list.append(aa_count_list)  # If want to debug, then replace the above line with this line
-        
+        #print(hyd_stdev_list)
+        stdev_out_list[1].append(statistics.stdev(hyd_stdev_list))  # add this pos's stdev to the 2nd row of the output which is the stdev value row
+        '''
+            For Debug purpose: replace the above line with this line (see the count of aa instead of pct)
+        fd_out_list.append(aa_count_list)  
+        '''
         i += 1
     del df  # clean the dataframe memory
-
-'''
-def cal_stdev(x):  # x = position_range   e.g. (1,549)
-    HYDROPATHY_SCORE_TABLE = {
-    'A': 0.68,
-    'C': 0.733,
-    'D': 0.19,
-    'E': 0.203,
-    'F': 1,
-    'G': 0.584,
-    'H': 0.304,
-    'I': 0.958,
-    'K': 0.403,
-    'L': 0.953,
-    'M': 0.782,
-    'N': 0.363,
-    'P': 0.759,
-    'Q': 0.376,
-    'R': 0.167,
-    'S': 0.466,
-    'T': 0.542,
-    'V': 0.854,
-    'W': 0.898,
-    'Y': 0.900,
-    'Z': 0,
-    '-': 1.5
-    }
-    print('calculating stdev') 
-    global stdev_out_list
-    stdev_out_list = []  # reset
-    header_row = seq_list[0]
-    #construct a dataframe for the selected lists
-    global df
-    df = pd.DataFrame(selection_list[1:], columns = header_row)
-    
-    stdev_out_list.append([f'{selection_value}_Position'] + AA + ['Sample#'])  # add headers for the outpur
-    
-    i = x[0]   # start from the first position in the position range
-    while ((i >= x[0]) and (i <= x[1])):
-        total_count = 0
-        aa_list = df[str(i)].tolist()  # use dataframe to extract the column of this position
-'''
-
 
 
 def zip_list(x,y):  # zip x then save to y
@@ -283,40 +275,45 @@ def zip_list(x,y):  # zip x then save to y
 
 def main():
     global fd_zip_output
-    global stdev_zip_output
-    if selection_name == group_attribute_name:
+    #global stdev_zip_output
+    if selection_name == group_attribute_name:  # calculate data based on group
         read_csv(seq_file, seq_list)
         generate_group_name_list()
         print(f"Original sequence file contains {len(seq_list)-1} samples")
         print(f"{len(group_name_list)} groups total\n")
+        all_group_stdev_output = []  # combine all group's stdev together into one file
         check_count = 0
         for i in group_name_list:
             select_rows(selection_name,i)
             if (len(selection_list)-1) <= (selection_value):  # cutoff, ignore low sample number groups
                 continue
             print(f"{i}: {len(selection_list)-1} samples are selected to calculate FD")
-            calculate_fd(position_range, i)
+            calculate_fd_stdev(position_range, i)
             fd_zip_output = []  # reset for each group's output
             zip_list(fd_out_list, fd_zip_output)   # zip the fd output
             write_csv(selection_list, f"{working_dir}Buffer({i}).csv")  # write the selection list file
             write_csv(fd_zip_output, f"{working_dir}FD({i}).csv")  # write fd output file
+            
+            all_group_stdev_output.append(stdev_out_list[1])  # only add the stdev value row
+            
             check_count += 1
         print(f"\nChecking ...... {check_count} groups are calculated. ")
-    else:
+        all_group_stdev_output.insert(0,stdev_out_list[0])  # add the header row
+        if need_stdev == True:
+            write_csv(all_group_stdev_output, stdev_out_file)  # write stdev output file
+            print("\n* StDev is also calculated in the same selected samples.   ")       
+    else:  # calculate data based on different attributes
         read_csv(seq_file, seq_list)
         select_rows(selection_name,selection_value)
         print(f"\nOriginal sequence file contains {len(seq_list)-1} samples")
         print(f"After the selection, {len(selection_list)-1} samples are selected to calculate FD")
-        if need_stdev == True:
-            calculate_fd(position_range, selection_value)
-            zip_list(fd_out_list, fd_zip_output)   # zip the fd output
-            write_csv(selection_list, buffer_file)  # write the selection list file
-            write_csv(fd_zip_output, fd_out_file)  # write fd output file
-        if need_stdev == False:
-            calculate_fd(position_range, selection_value)
-            zip_list(fd_out_list, fd_zip_output)  # zip the fd output
-            write_csv(selection_list, buffer_file)  # write the selection list file
-            write_csv(fd_zip_output, fd_out_file)  # write fd output file
+        calculate_fd_stdev(position_range, selection_value)
+        zip_list(fd_out_list, fd_zip_output)  # zip the fd output
+        write_csv(selection_list, buffer_file)  # write the selection list file
+        write_csv(fd_zip_output, fd_out_file)  # write fd output file
+        if need_stdev == True:  # calculate stdev
+            write_csv(stdev_out_list, stdev_out_file)  # write stdev output file
+            print("\n* StDev is also calculated in the same selected samples.   ")            
 
 
 main()
