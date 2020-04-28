df007e331c5b3742d5939d48cad06d3278412a14
MrColinHan <changzehan@yahoo.com>
2020-04-28 13:12:54 -0500

    new feature: check for nucleptide segment

--- SARS-CoV-2/step2-CleanUp-FilterSequence.py
+++ SARS-CoV-2/step2-CleanUp-FilterSequence.py
@@ -7,12 +7,9 @@ from re import compile
 import csv
 
 """
-    This program clean up the extracted SARS-Cov-2 nucleotide sequences after step1. 
+    This program filter the extracted SARS-Cov-2 nucleotide sequences after step1. 
     
-    Standard sequence(numbering): MN908947, length 21291
-    
-    Remove : sequences that are too short (< 20000)
-             sequences contain too many non-ACTG
+    Output the sequences with nucleotide segment "GAATGACAAAA"
     
     Input: fasta
     Output: fasta
@@ -20,19 +17,16 @@ import csv
 """
 
 # ==========================================================================================
-working_dir = r"/Users/Han/Documents/Haim_Lab(2018_summer)/4.26.20_sars_cov-2/"
+working_dir = r"/Users/Han/Documents/Haim_Lab(2018_summer)/4.26.20_sars_cov-2/complete_record/"
 in_fas_name = "complete_record(extracted_1524).fasta"
 out_name = "complete_record(extracted_filtered_).fasta"
 
-length_cutoff = 20000  # if the sequence length < 20000, then remove
-non_ACTG_cutoff = 1  # if the sequence contain >1 non-ACTG, then remove
+nucleo_segment = "GAATGACAAAA"
 # ==========================================================================================
 input_file = working_dir + in_fas_name
 input_list = []  # save input sequences
 output_file = working_dir + out_name
 output_list = []  # save output sequences
-
-nucleotide_seq = ('A', 'C', 'T', 'G', '-', ' ', '\n')
 
 
 def readFasta(x, y):  # read a fasta file x and store into a list y
@@ -48,32 +42,53 @@ def write(x, y):  # write the list x into y file
     output.close
 
 
-def write_csv(x, y):  # write list x into file y
-    with open(y, 'w+') as file:
-        wr = csv.writer(file, dialect='excel')
-        wr.writerows(x)
-    file.close()
-
-
 # l is the input list or the remaining of input list. This program extract the first set of sequence.
 def find_next_sequence_set(l):
     result = []
-    for row_i in range(len()):
-        if
-
-    return result
+    if l[0][0] == '>':
+        result.append(l[0])
+        for row in l[1:]:
+            if row[0].isalpha() and row[0].isupper():
+                result.append(row)
+            else:  # row[0] is '>'
+                break  # break before next set of sequence
+    else:  # first row is not accession number
+        raise Exception  # stop the program
+    return result  # return the sequence block
 
 def main():
+    global input_list
+    global output_list
+
     readFasta(input_file, input_list)
+
+    test_list = []  # check whether test_list == input_list
+    test_count = 0
+    count_after_filter = 0  # keep track of how many are selected after filter
 
     current_index = 0  # index of current row
     current_sequence = []
-    while current_index < len()
-    for row in input_list:
-        if row[0] == '>':
-            return
-        else:
+    while current_index < len(input_list):
+        print(current_index)
+        current_sequence = find_next_sequence_set(input_list[current_index:])  # update current sequence
+        test_list += current_sequence  # check
+        test_count += 1  # check
 
+        current_seq_string = ""  # store the sequence of current sequence in order to test nucleo_seg
+        for row in current_sequence[1:]:  # exclude accession number row
+            for nucl in row:
+                if nucl.isalpha() and nucl.isupper():
+                    current_seq_string += nucl
+
+        if nucleo_segment in current_seq_string:
+            output_list += current_sequence
+            count_after_filter += 1
+
+        current_index += len(current_sequence)
+
+    print(f"\ncheck: 'test_list == input_list' : {test_list == input_list}")
+    print(f"check: test_count: {test_count}")
+    print(f"\nAfter filter '{nucleo_segment}' : {count_after_filter} are written into output")
 
     write(output_list, output_file)
 
