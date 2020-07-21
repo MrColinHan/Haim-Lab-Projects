1dfc6d8f936abdded8301febdb459dacac6536e0
MrColinHan <changzehan@yahoo.com>
2020-07-21 15:48:15 -0500

    add new folder and script: process Primer-ID sequences

---
+++ HIV/Primer-ID_Process/fastq_to_fasta.py
@@ -0,0 +1,12 @@
+"""
+Created on Jul 21 2020
+
+@author: Changze Han
+"""
+
+'''
+    For HIV Primer-ID project: 
+        1. fastq files are downloaded from NCBI website "https://www.ncbi.nlm.nih.gov/bioproject/?term=PRJNA356756" 
+           Download process requires the usage of tool "seatoolkit" from NCBI
+        2. this script convert fastq into fasta in order to read them in MEGA software
+'''
