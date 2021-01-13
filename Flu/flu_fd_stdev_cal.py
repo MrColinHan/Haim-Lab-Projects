b4cc321938d6a6b51510ecea8cecd32bcfa4b3ff
MrColinHan <changzehan@yahoo.com>
2021-01-13 12:43:48 -0600

    Update commens a bit & sync args

--- Flu/flu_fd_stdev_cal.py
+++ Flu/flu_fd_stdev_cal.py
@@ -34,9 +34,10 @@ import numpy as np
 #from constants import HYDROPATHY_SCORE_TABLE
 
 # ==========================================================================================
-working_dir = r"/Users/Han/Documents/Haim_Lab(2018_summer)/9.25.19_new_accession(H1N1_USA)/06-19_season/season_FDs/"
-seq_filename = r"06-19_season_H1N1_USA_AA_PNGS_10375(&all_attributes).csv"
-fd_output_filename = r"18-19_FD.csv"
+working_dir = r"/Users/Han/Documents/Haim_Lab(2018_summer)/1.12.21_flu_tree/fd&std_output/"
+#r"/Users/Han/Documents/Haim_Lab(2018_summer)/9.25.19_new_accession(H1N1_USA)/06-19_season/season_FDs/"
+seq_filename = r"segment_3.csv"
+fd_output_filename = r"10-19_FD.csv"
 # if selection_name is group, then 1. this fd output file name doesn't matter 
 #                                  2. make sure to make a new folder to perfrom FD for group selection(due to large amount of outputs)
 stdev_output_filename = r"test_stdev.csv"  
@@ -46,7 +47,7 @@ stdev_output_filename = r"test_stdev.csv"
 #option 1:
 season_attribute_name = 'Flu Season'  # this is the name of season attribute appears in the sequence file
 #option 2:
-group_attribute_name = 'Flu Season(group)' #'Patient-Days'#'Group'  # this is the name of group attribute appears in the sequence file
+group_attribute_name = 'tree_segment' #'Patient-Days'#'Group'  # this is the name of group attribute appears in the sequence file
 #option 3:
 state_attribute_name = 'State/Province'  # this is the name of state attribute appears in the sequence file
 #option 4:
@@ -59,17 +60,17 @@ selection_name = group_attribute_name  # choose from 5 options above
 # select cutoff/value/... : 
 selection_value = 0 #'15-16'
 # 1. if 'Flu Season' is selected, you can also do multiple 
-#    years : '13-16' which will combine 13-14,14-15,15-16
-# 2. if 'Group' is selected, then selection_value is the cutoff value, 
+#    years : type '13-16' and it will combine 13-14,14-15,15-16
+# 2. if 'Group' is selected, then selection_value is the group size cutoff value,
 #    cutoff meaning: ignore groups that have sample numbers <= cutoff value
 # 3. if 'Sequence Accession' is selected, then the selection_value is a list of accession numbers, 
 #      e.g.: ['KU591055', 'KU591039', 'KU590350']
 
 # remember to change the position range based on different flu type
 # e.g: H1N1--(1,549)   H3N2--(1,550)
-position_range = (1, 549)
+position_range = (1, 550)
  
-need_stdev = False  # True : if want to calculate stdev  False: only calculate FD
+need_stdev = True  # True : if want to calculate stdev  False: only calculate FD
 zero_thresh = float('1e-5')  # stdev values < zero_thresh will be assigned zero
 # ==========================================================================================
 
